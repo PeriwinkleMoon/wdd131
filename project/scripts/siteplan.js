834192c41dd8a9a330e9a47454223c08fdc696d6
PeriwinkleMoon <sunrisesunsetforever@gmail.com>
2024-07-08 15:53:50 -0600

    Site Plan css and js edit

---
+++ project/scripts/siteplan.js
@@ -0,0 +1,12 @@
+// GET DATES
+const year = document.querySelector("#currentyear");
+
+const today = new Date();
+
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+const modPara = document.querySelector("#lastModified");
+let oLastModif = new Date(document.lastModified);
+let nLastModif = Date.parse(document.lastModified);
+
+modPara.innerHTML = `Last Modification: ${oLastModif}`
